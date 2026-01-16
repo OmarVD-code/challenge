cb62f00651f72a01ec32330807fd64d45369d067
OmarVD-code <omarvillarreal2000@gmail.com>
2026-01-15 21:03:10 -0500

    challenge 2

--- game-02/src/GildedRose.php
+++ game-02/src/GildedRose.php
@@ -11,57 +11,51 @@ final class GildedRose
      */
     public function __construct(
         private array $items
-    ) {
-    }
+    ) {}
 
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
+            if ($item->name === 'Sulfuras, Hand of Ragnaros') {
+                continue;
+            }
+
+            if ($item->name === 'Aged Brie') {
+                $this->increaseQuality($item, $item->sellIn <= 0 ? 2 : 1);
+            } elseif ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->sellIn <= 0) {
+                    $item->quality = 0;
+                } else {
+                    $inc = 1;
+                    if ($item->sellIn <= 10) $inc++;
+                    if ($item->sellIn <= 5)  $inc++;
+                    $this->increaseQuality($item, $inc);
                 }
             } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
+                $degrade = 1;
+
+                if (str_contains($item->name, 'Conjured')) {
+                    $degrade *= 2;
                 }
+
+                if ($item->sellIn <= 0) {
+                    $degrade *= 2;
+                }
+
+                $this->decreaseQuality($item, $degrade);
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+            $item->sellIn -= 1;
         }
+    }
+
+    private function increaseQuality(Item $item, int $amount): void
+    {
+        $item->quality = min(50, $item->quality + $amount);
+    }
+
+    private function decreaseQuality(Item $item, int $amount): void
+    {
+        $item->quality = max(0, $item->quality - $amount);
     }
 }
